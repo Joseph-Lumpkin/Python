aed5f908c9ce056f8a2011d2cdbb38ab62df43cd
Joseph-Lumpkin <Joseph-Lumpkin@users.noreply.github.com>
2019-06-25 21:15:29 -0400

    Update Clipboard Extractor.py

--- Learning/regex/Clipboard Extractor.py
+++ Learning/regex/Clipboard Extractor.py
@@ -5,9 +5,9 @@ import pyperclip as pc, re
 phoneRegex = re.compile(r'''(
 		(\d{3}|\(\d{3}\))? 	 	 	 	# area code
 		(\s|-|\.)? 	 	 	 	 	 	# separator
-		(\d{3}) 	 	 	 	 	 	 	# first 3 digits
+		(\d{3}) 	 	 	 	 	 	# first 3 digits
 		(\s|-|\.)? 	 	 	 	 	 	# separator
-		(\d{4}) 	 	  	 	 	 	 	# last 4 digits
+		(\d{4}) 	 	  	 	 	 	# last 4 digits
 		(\s*(ext|x|ext.)\s*(\d{2,5}))? 	# extension
 		)''', re.VERBOSE)
 
@@ -15,7 +15,7 @@ phoneRegex = re.compile(r'''(
 #Email regex
 emailRegex = re.compile(r'''(
 		[a-zA-Z0-9._%+-]+ 	 	# Username
-		@ 	 	 	 	 	 	 	# @ symbol
+		@ 	 	 	 	 	 	# @ symbol
 		[a-zA-Z0-9.-]+ 	 	 	# Domain name
 		(\.[a-zA-Z]{2,4}) 	 	# Top level domain
 		)''', re.VERBOSE)
