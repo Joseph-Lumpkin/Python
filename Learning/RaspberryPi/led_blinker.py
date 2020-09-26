665da63e8a6009d8f48d31091d16f463058dab73
Joseph-Lumpkin <nictus1938@gmail.com>
2020-09-26 18:51:31 -0400

    Update led_blinker.py

--- Learning/RaspberryPi/led_blinker.py
+++ Learning/RaspberryPi/led_blinker.py
@@ -6,8 +6,8 @@ GPIO.setup(7, GPIO.OUT)
 
 for i in range(50):
     GPIO.output(7, True)
-    time.sleep(1)
+    time.sleep(.1)
     GPIO.output(7, False)
-    time.sleep(1)
+    time.sleep(.1)
     
 GPIO.cleanup()
