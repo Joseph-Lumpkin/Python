f7202eb62ed48b6783458a2269b6e6877f0e3c7b
Joe Laptop <nictus1938@gmail.com>
2020-09-27 10:57:18 -0400

    Update door_chime.py

--- Learning/RaspberryPi/door_chime.py
+++ Learning/RaspberryPi/door_chime.py
@@ -11,12 +11,12 @@ pygame.mixer.music.load("renai_circulation.mp3")
 
 try:
     while True:
-        if GPIO.input(7) == False:
-            if GPIO.input(37) == False:
-                print("Door Closed = true, stopping music")
-                pygame.mixer.music.stop()
         if GPIO.input(7) == True:
             if GPIO.input(37) == True:
+                print("Door Closed = true, stopping music")
+                pygame.mixer.music.stop()
+        if GPIO.input(7) == False:
+            if GPIO.input(37) == False:
                 if pygame.mixer.music.get_busy() == False:
                     pygame.mixer.music.stop()
                     pygame.mixer.music.load("renai_circulation.mp3")
