9dbcf6aca87e18af45dec2090c10b6147032ae22
Joseph-Lumpkin <nictus1938@gmail.com>
2020-09-26 20:02:24 -0400

    Door Chime

---
+++ Learning/RaspberryPi/door_chime.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+from playsound import playsound
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN)
+
+
+try:
+    while True:
+        if GPIO.input(7):
+            continue
+        if GPIO.input(7) == False:
+            playsound('res/renai_circulation')
+        time.sleep(10);
+finally:
+    GPIO.cleanup()
