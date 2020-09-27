e96acf045cbd7a2ed3ead29e0c0c278fea24c2d9
Joseph-Lumpkin <nictus1938@gmail.com>
2020-09-26 20:23:43 -0400

    Update door_chime.py

--- Learning/RaspberryPi/door_chime.py
+++ Learning/RaspberryPi/door_chime.py
@@ -1,17 +1,20 @@
 import RPi.GPIO as GPIO
 import time
-from playsound import playsound
+import pygame
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN)
-
+pygame.mixer.init()
+pygame.mixer.music.load('renai_circulation.mp3')
 
 try:
     while True:
         if GPIO.input(7):
+            pygame.mixer.music.stop
             continue
         if GPIO.input(7) == False:
-            playsound('res/renai_circulation.mp3')
-        time.sleep(10);
+            if pygame.mixer.music.get_busy() == True:
+                continue
+            pygame.mixer.music.play()
 finally:
     GPIO.cleanup()
