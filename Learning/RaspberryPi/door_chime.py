32ac56c1661b61dbe36173170f195ff18dbb9fbe
Joseph-Lumpkin <nictus1938@gmail.com>
2020-09-27 08:40:59 -0400

    Update door_chime.py

--- Learning/RaspberryPi/door_chime.py
+++ Learning/RaspberryPi/door_chime.py
@@ -1,44 +1,23 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+import pygame
 import time
-import pyaudio
-import wave
-import sys
 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(7, GPIO.IN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN)
+pygame.mixer.init()
+pygame.mixer.music.load("res/renai_circulation.mp3")
 
-CHUNK = 1024
-wf = wave.open(sys.argv[1], 'rb')
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-data = wf.readframes(CHUNK)
-
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
-
-#time.sleep(10)
-
-# try:
-#     while True:
-#         if GPIO.input(7) == True:
-#             print("input = true")
-#             pygame.mixer.music.stop()
-#         elif GPIO.input(7) == False:
-#             pygame.mixer.Sound.play(door_chime)
-#             print("input = false, music should be playing")
-
-#         time.sleep(5)
-# finally:
-#     GPIO.cleanup()
+try:
+    while True:
+        if GPIO.input(7) == True:
+            print("input = true")
+            pygame.mixer.music.stop()
+        elif GPIO.input(7) == False:
+            if pygame.mixer.music.get_busy() == False:
+                pygame.mixer.music.stop()
+                pygame.mixer.music.load("renai_circulation.mp3")
+                pygame.mixer.music.play()
+                print("input = false, music should be playing")
+        time.sleep(5)
+finally:
+    GPIO.cleanup()
